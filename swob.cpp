5ebb8f1ecee4bbb2e059e492ed7ac05b213cf2da
deanturpin <git@brightcoin.uk>
2018-05-30 17:01:42 +0100

    Reword

--- swob.cpp
+++ swob.cpp
@@ -3,16 +3,17 @@
 
 int main() {
 
-  // A project needs a name and a list of comma separated tools
+  // A project needs a name and a list of attributes, the tools are
+  // comma-separated, the version and tool name is delimited by a space
   struct project {
-    std::string name {"no name"};
-    std::string toolchain {"Windows 10, gcc 5, RHEL 5"};
+    const std::string name{"no name"};
+    const std::string toolchain{"Windows 10, gcc 5, RHEL 5"};
   };
 
   // Create some projects with toolchains
-  const std::vector<project> projects {
-    {"Dean's laptop", "Ubuntu 17, gcc 6, clang 4"},
-    {"Dean's Travis build", "Ubuntu 14, gcc 8, clang 6"},
+  const std::vector<project> projects{
+      {"Dean's laptop", "Ubuntu 17, gcc 6, clang 4"},
+      {"Dean's Travis build", "Ubuntu 14, gcc 8, clang 6"},
   };
 
   // Summary
