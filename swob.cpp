1f09e9e6225e5a1fb0fe47e152e047152c92b3e4
deanturpin <kali@brightcoin.uk>
2018-07-12 18:26:04 +0100

    Use init lists to create projects

--- swob.cpp
+++ swob.cpp
@@ -4,66 +4,74 @@
 #include <iterator>
 #include <numeric>
 #include <sstream>
+#include <tuple>
 #include <vector>
 
 // A project needs a name and a list of attributes of the format:
-struct project {
-  std::string name;
-  std::vector<std::string> toolchain;
+// struct project {
+//   std::string name;
+//   std::vector<std::string> toolchain;
+// 
+//   friend std::istream &operator>>(std::istream &in, project &p) {
+//     p.toolchain = {std::istream_iterator<std::string>(in), {}};
+//     return in;
+//   }
+// };
+// 
+// template <typename T = project> auto parse_config(const std::string file) {
+// 
+//   std::vector<T> projects;
+//   std::ifstream project_file(file);
+//   std::string line;
+//   project proj;
+// 
+//   while (getline(project_file, line)) {
+// 
+//     // Skip blank lines
+//     if (!line.empty()) {
+// 
+//       // Store the unpopulated param
+//       std::stringstream ss(line);
+//       if (proj.name.empty())
+//         proj.name = line;
+//       else
+//         ss >> proj;
+//     }
+// 
+//     // If project is complete store it and clear down local
+//     if (!proj.toolchain.empty()) {
+//       projects.emplace_back(proj);
+//       proj.name.clear();
+//       proj.toolchain.clear();
+//     }
+//   }
+// 
+//   return projects;
+// }
 
-  friend std::istream &operator>>(std::istream &in, project &p) {
-    p.toolchain = {std::istream_iterator<std::string>(in), {}};
-    return in;
-  }
+struct project2 {
+  std::string name;
+  std::vector<std::pair<std::string, std::string>> toolchain;
 };
 
-template <typename T = project> auto parse_config(const std::string file) {
-
-  std::vector<T> projects;
-  std::ifstream project_file(file);
-  std::string line;
-  project proj;
-
-  while (getline(project_file, line)) {
-
-    // Skip blank lines
-    if (!line.empty()) {
-
-      // Store the unpopulated param
-      std::stringstream ss(line);
-      if (proj.name.empty())
-        proj.name = line;
-      else
-        ss >> proj;
-    }
-
-    // If project is complete store it and clear down local
-    if (!proj.toolchain.empty()) {
-      projects.emplace_back(proj);
-      proj.name.clear();
-      proj.toolchain.clear();
-    }
-  }
-
-  return projects;
-}
+const std::vector<project2> _p {
+	{"Dean_laptop", {{"kali", "2018.2"}, {"gcc", "8"}, {"clang", "6"}}},
+	{"Dean Travis", {{"Ubuntu", "14"}, {"gcc", "6"}, { "kernel", "4.14"}, {"clang", "6"}, {"C++", "14"}}},
+};
 
 int main() {
 
   // Read project and tool info
-  const auto blah = parse_config("projects.txt");
-  const auto projects = std::move(blah);
-  const auto &tools = parse_config("tools.txt");
+  // const auto &tools = parse_config("tools.txt");
 
-  // Print summary
-  for (const auto &type : {tools, projects})
-    for (const auto &i : type) {
-      std::cout << i.name << '\n';
-
-      for (const auto &t : i.toolchain)
-	      std::cout << t << ' ';
-      std::cout << '\n';
-     }
+  for (const auto &project : _p) { // parse_config("projects.txt")) {
+	  std::cout << project.name << '\n';
+		for (const auto &tool : project.toolchain) {
+			std::string tool_name, revision;
+			std::tie(tool_name, revision) = tool;
+			std::cout << '\t' << tool_name << '\t' << revision << '\n';
+		}
+  }
 
   // std::stringstream summary;
   // for (const auto &p : projects) {
